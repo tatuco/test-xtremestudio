06c1e46244fb30540aa77e0e09a9e60ebf74687f
Ysrael Mendez <ysmendezm@gmail.com>
2019-01-31 15:09:04 -0300

    Update dashboard API #7

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,64 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>API - Yo Planifico</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
+        <link href="{{ asset('css/welcome.css') }}" rel='stylesheet'>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -70,7 +18,7 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <!--<a href="{{ route('login') }}">Login</a>-->
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
@@ -81,8 +29,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Pichurria
-                </div>
+                  <img src="{{ asset('images/logocentinela.png') }}"  class="centinela-img">
+                  <img src="{{ asset('images/yoplanifico.png') }}"  class="yoplanifico-img">
+            </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
