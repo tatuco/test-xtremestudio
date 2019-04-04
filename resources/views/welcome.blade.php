aff20cdaf87207d645a3ff61e290775a08766d12
Ysrael <ysmendezm@gmail.com>
2019-04-04 01:28:42 -0300

    Test index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,12 +35,11 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://yoplanifico.cl">Yo Planifico</a>
+                    <a href="http://www.aminerals.cl/">Antofasta Minerals</a>
                     <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://blog.laravel.com">Plazfer Spa</a>
+                    <a href="https://github.com/plazfer">GitHub</a>
                 </div>
             </div>
         </div>
