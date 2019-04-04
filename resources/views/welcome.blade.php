334d1c31acb4412cd6b6ee4d33ae665e7289fa11
Ysrael <ysmendezm@gmail.com>
2019-04-04 01:05:03 -0300

    Secure_route asset index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-        <link rel="icon" type="image/x-icon" href="{{ URL::to('images/favicon.ico') }}">
-        <link href="{{ URL::to('css/welcome.css') }}" rel='stylesheet'>
+        <link rel="icon" type="image/x-icon" href="{{ secure_asset('images/favicon.ico') }}">
+        <link href="{{ secure_asset('css/welcome.css') }}" rel='stylesheet'>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -29,8 +29,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                  <img src="{{ URL::to('images/logocentinela.png') }}"  class="centinela-img">
-                  <img src="{{ URL::to('images/yoplanifico.png') }}"  class="yoplanifico-img">
+                  <img src="{{ secure_asset('images/logocentinela.png') }}"  class="centinela-img">
+                  <img src="{{ secure_asset('images/yoplanifico.png') }}"  class="yoplanifico-img">
             </div>
 
                 <div class="links">
