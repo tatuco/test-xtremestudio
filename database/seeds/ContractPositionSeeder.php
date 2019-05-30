ae2f6d75eba06b6ca65fada12e9654538f5ed17d
Luis <luisjrm936@gmail.com>
2019-05-30 12:43:45 -0500

    añadiendo seeder con data de prueba para la tabla pivote entra contrato y cargo

--- database/seeds/ContractPositionSeeder.php
+++ database/seeds/ContractPositionSeeder.php
@@ -11,35 +11,35 @@ class ContractPositionSeeder extends Seeder
      */
     public function run()
     { //NEW-----------------------------------------------
-        DB::table('contracts')->insert([
+        DB::table('position_company_pivot_contracts')->insert([
             'contract_id' => '4540002894',
             'position_id' => 3,
             'deleted'=> FALSE,
             'created_at'=> new DateTime,
             'updated_at'=> new DateTime
         ]);
-        DB::table('contracts')->insert([
+        DB::table('position_company_pivot_contracts')->insert([
             'contract_id' => '4540002870',
             'position_id' => 2,
             'deleted'=> FALSE,
             'created_at'=> new DateTime,
             'updated_at'=> new DateTime
         ]);
-        DB::table('contracts')->insert([
+        DB::table('position_company_pivot_contracts')->insert([
             'contract_id' => '4540002892',
             'position_id' => 1,
             'deleted'=> FALSE,
             'created_at'=> new DateTime,
             'updated_at'=> new DateTime
         ]);
-        DB::table('contracts')->insert([
+        DB::table('position_company_pivot_contracts')->insert([
             'contract_id' => '4540003012',
             'position_id' => 2,
             'deleted'=> FALSE,
             'created_at'=> new DateTime,
             'updated_at'=> new DateTime
         ]);
-        DB::table('contracts')->insert([
+        DB::table('position_company_pivot_contracts')->insert([
             'contract_id' => '4540003013',
             'position_id' => 1,
             'deleted'=> FALSE,
