60d0bcd2db9f873b49e8d33ab0ed2d9e0f6bd3ce
Luis <luisjrm936@gmail.com>
2019-06-20 13:17:46 -0500

    configurqacion para heroku prueba luis laura

--- database/migrations/2019_05_15_193716_drop_columns_contract_company_position_company_table.php
+++ database/migrations/2019_05_15_193716_drop_columns_contract_company_position_company_table.php
@@ -12,12 +12,7 @@ class DropColumnsContractCompanyPositionCompanyTable extends Migration
      */
     public function up()
     {
-        //Schema::create('position_companies', function (Blueprint $table) {
-          //  $table->dropColumn('company_id');
-           // $table->dropColumn('contract_id');
-        //});
-      //  PDO::prepare("ALTER TABLE position_companies DROP COLUMN company_id");
-      //  PDO::prepare("ALTER TABLE position_companies DROP COLUMN contract_id");
+
         DB::connection()->getPdo()->exec("ALTER TABLE position_companies DROP FOREIGN KEY position_companies_company_id_foreign");
         DB::connection()->getPdo()->exec("ALTER TABLE position_companies DROP FOREIGN KEY position_companies_contract_id_foreign");
         DB::connection()->getPdo()->exec("ALTER TABLE position_companies DROP COLUMN company_id");
